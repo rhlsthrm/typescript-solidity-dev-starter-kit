91dc0914da22616fc63779c1364fd7c468a87696
Rahul <rksethuram9@gmail.com>
2019-10-29 20:16:24 +0100

    Add Etherscan plugin

--- buidler.config.ts
+++ buidler.config.ts
@@ -2,9 +2,11 @@ import { BuidlerConfig, usePlugin } from "@nomiclabs/buidler/config";
 import waffleDefaultAccounts from "ethereum-waffle/dist/config/defaultAccounts";
 
 usePlugin("@nomiclabs/buidler-ethers");
+usePlugin("@nomiclabs/buidler-etherscan");
 
 const INFURA_API_KEY = "";
 const RINKEBY_PRIVATE_KEY = "";
+const ETHERSCAN_API_KEY = "";
 
 const config: BuidlerConfig = {
   solc: {
@@ -24,6 +26,13 @@ const config: BuidlerConfig = {
       url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [RINKEBY_PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    // The url for the Etherscan API you want to use.
+    url: "https://api-rinkeby.etherscan.io/api",
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
   }
 };
 
