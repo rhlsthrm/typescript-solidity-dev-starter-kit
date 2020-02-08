f2bd868606742136c8555076f9306ba539e3a12b
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-02-08 17:37:03 +0100

    Update deps

--- buidler.config.ts
+++ buidler.config.ts
@@ -11,7 +11,7 @@ const ETHERSCAN_API_KEY = "";
 
 const config: BuidlerConfig = {
   solc: {
-    version: "0.5.12"
+    version: "0.6.2"
   },
   paths: {
     artifacts: "./build"
--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.12;
+pragma solidity ^0.6.2;
 
 contract Counter {
   uint256 count = 0;
