1d25eb7ea0b3707137a2d612a6d158e1d0c6c5e3
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2021-06-23 16:28:30 -0600

    Improve deployment script messages
    Explain to user what the output they're seeing is.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,14 +6,21 @@ async function main() {
   // If we had constructor arguments, they would be passed into deploy()
   let contract = await factory.deploy();
 
-  // The address the Contract WILL have once mined
-  console.log(contract.address);
+  console.log(
+      `The address the Contract WILL have once mined: ${contract.address}`
+  );
 
-  // The transaction that was sent to the network to deploy the Contract
-  console.log(contract.deployTransaction.hash);
+  console.log(
+      `The transaction that was sent to the network to deploy the Contract: ${
+          contract.deployTransaction.hash
+      }`
+  );
 
-  // The contract is NOT deployed yet; we must wait until it is mined
+  console.log(
+      'The contract is NOT deployed yet; we must wait until it is mined...'
+  );
   await contract.deployed();
+  console.log('Mined!');
 }
 
 main()
