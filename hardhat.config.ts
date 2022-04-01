509197a0b0358a5a48047df12adfe48e7ad5f429
Diadal <info@diadal.com.ng>
2022-04-01 00:40:17 -0700

    Update hardhat.config.ts
    fix etherscan typescript error

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,13 +8,20 @@ import "@typechain/hardhat";
 import "@nomiclabs/hardhat-etherscan";
 import "solidity-coverage";
 
+interface Etherscan {
+  etherscan: { apiKey: string | undefined };
+}
+
+type HardhatUserEtherscanConfig = HardhatUserConfig & Etherscan;
+
+
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const RINKEBY_PRIVATE_KEY =
   process.env.RINKEBY_PRIVATE_KEY! ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
-const config: HardhatUserConfig = {
+const config: HardhatUserEtherscanConfig = {
   defaultNetwork: "hardhat",
   solidity: {
     compilers: [{ version: "0.7.6", settings: {} }],
